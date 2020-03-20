e3932a8384bd7415e96fecf000e33c6833c2eecd
Albert J. Wong <ajwong@chromium.org>
2020-03-20 16:39:29 -0700

    Make reload endpoint also autogenerate HTML fragment

--- functions/index.js
+++ functions/index.js
@@ -76,12 +76,117 @@ async function getSpreadsheet(client) {
   return response.data;
 }
 
-// HTTPS function to write new data to CONFIG_DATA_PATH, for testing
-exports.reloadsheetdata = functions.https.onRequest(async (req, res) => {
+async function snapshotData(filename) {
   // Talk to sheets.
   const client = await getAuthorizedClient();
   // Talk to sheets.
   const data = await getSpreadsheet(client);
+  const datafileRef = admin.storage().bucket().file(filename);
 
-  res.send('done');
+  await datafileRef.save(JSON.stringify(data), {
+    gzip: true,
+    metadata: {
+      cacheControl: "public, max-age=300",
+      contentType: "application/json"
+    },
+    predefinedAcl: "publicRead",
+  });
+
+  return data;
+}
+
+function toDataByLocation(data) {
+  const headers = data.values[0];
+  const approvedIndex = headers.findIndex( e => e === 'Approved' );
+  const stateIndex = headers.findIndex( e => e === 'State?' );
+  const cityIndex = headers.findIndex( e => e === 'City' );
+  const data_by_location = {};
+
+  const published_entries = data.values.slice(1).filter((entry) => entry[approvedIndex] === "x");
+
+  published_entries.forEach( entry => {
+    const state = entry[stateIndex];
+    const city = entry[cityIndex];
+    let entry_array;
+    if (!(state in data_by_location) || !(city in data_by_location[state])) {
+      entry_array = [];
+      if (state in data_by_location) {
+        data_by_location[state][city] = entry_array;
+      } else {
+        data_by_location[state] = { [city]: entry_array };
+      }
+    } else {
+      entry_array = data_by_location[state][city];
+    }
+    const entry_obj = {};
+    headers.forEach( (value, index) => {
+      if (entry[index] !== undefined) {
+        entry_obj[value] = entry[index]
+      } else {
+        entry_obj[value] = ""
+      }
+    });
+    entry_array.push(entry_obj);
+  });
+
+  return data_by_location;
+}
+
+function toHtmlSnippets(data_by_location) {
+   let padding = '      ';
+  const lines = [ `${padding}<article>` ];
+  padding += '  ';
+
+  lines.push(`${padding}<h1>List of donation sites</h1>`);
+  padding += '  ';
+
+  for (const state of Object.keys(data_by_location).sort()) {
+    lines.push(`${padding}<h2>${state}</h2>`);
+    padding += '  ';
+
+    const cities = data_by_location[state];
+    for (const city of Object.keys(cities).sort()) {
+      lines.push(`${padding}<h3>${city}</h3>`);
+      padding += '  ';
+
+      for (const entry of cities[city]) {
+        const name = entry["What is the name of the hospital or clinic?"];
+        const address = entry["Street address for dropoffs?"];
+        const instructions = entry["Drop off instructions, eg curbside procedure or mailing address ATTN: instructions:"];
+        const accepting = entry["What are they accepting?"];
+        const will_they_accept = entry["Will they accept open boxes/bags?"];
+
+        lines.push(`${padding}<h4 class="marginBottomZero">${name}</h4>`);
+        padding += '  ';
+
+        lines.push(`${padding}<p class="marginTopZero medEmph">${address.replace(/\n/g,'<br>')}</p>`);
+        if (instructions !== "") {
+          lines.push(`${padding}<p">${instructions}</p>`);
+        }
+        if (accepting !== "") {
+          lines.push(`${padding}<p">${accepting}</p>`);
+        }
+        if (will_they_accept !== "") {
+          lines.push(`${padding}<p">${will_they_accept}</p>`);
+        }
+
+        padding = padding.substring(0, padding.length - 2);
+      }
+
+      padding = padding.substring(0, padding.length - 2);
+    }
+
+    padding = padding.substring(0, padding.length - 2);
+  }
+  lines.push('      </article>');
+  return lines;
+}
+
+const DATA_FILE_NAME = 'data.json';
+exports.reloadsheetdata = functions.https.onRequest(async (req, res) => {
+  const data = await snapshotData(DATA_FILE_NAME);
+  const data_by_location = toDataByLocation(data);
+  const html_snippets = toHtmlSnippets(data_by_location);
+
+  res.status(200).send(html_snippets.join("\n"));
 });
