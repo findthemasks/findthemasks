635f0e76bac3d1623de6a197d937771eaae0eedb
Albert J. Wong <ajwong@chromium.org>
2020-03-19 22:17:31 -0700

    Spreadsheet scrape endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,91 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const {OAuth2Client} = require('google-auth-library');
+const {google} = require('googleapis');
+
+// TODO: Use firebase functions:config:set to configure your googleapi object:
+// googleapi.client_id = Google API client ID,
+// googleapi.client_secret = client secret, and
+// googleapi.sheet_id = Google Sheet id (long string in middle of sheet URL)
+const CONFIG_CLIENT_ID = functions.config().googleapi.client_id;
+const CONFIG_CLIENT_SECRET = functions.config().googleapi.client_secret;
+const CONFIG_SHEET_ID = functions.config().googleapi.sheet_id;
+
+// The OAuth Callback Redirect.
+const FUNCTIONS_REDIRECT = `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/oauthcallback`;
+
+// setup for authGoogleAPI
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const functionsOauthClient = new OAuth2Client(CONFIG_CLIENT_ID, CONFIG_CLIENT_SECRET,
+  FUNCTIONS_REDIRECT);
+
+// OAuth token cached locally.
+let oauthTokens = null;
+
+// visit the URL for this Function to request tokens
+exports.authgoogleapi = functions.https.onRequest((req, res) => {
+  res.set('Cache-Control', 'private, max-age=0, s-maxage=0');
+  res.redirect(functionsOauthClient.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+    prompt: 'consent',
+  }));
+});
+
+// setup for OauthCallback
+const DB_TOKEN_PATH = '/api_tokens';
+
+// after you grant access, you will be redirected to the URL for this Function
+// this Function stores the tokens to your Firebase database
+exports.oauthcallback = functions.https.onRequest(async (req, res) => {
+  res.set('Cache-Control', 'private, max-age=0, s-maxage=0');
+  const code = req.query.code;
+  try {
+    const {tokens} = await functionsOauthClient.getToken(code);
+    // Now tokens contains an access_token and an optional refresh_token. Save them.
+    await admin.database().ref(DB_TOKEN_PATH).set(tokens);
+    return res.status(200).send('App successfully configured with new Credentials. '
+        + 'You can now close this page.');
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+// checks if oauthTokens have been loaded into memory, and if not, retrieves them
+async function getAuthorizedClient() {
+  if (oauthTokens) {
+    return functionsOauthClient;
+  }
+  const snapshot = await admin.database().ref(DB_TOKEN_PATH).once('value');
+  oauthTokens = snapshot.val();
+  functionsOauthClient.setCredentials(oauthTokens);
+  return functionsOauthClient;
+}
+
+function getSpreadsheet(client) {
+  return new Promise((resolve, reject) => {
+    const sheets = google.sheets('v4');
+    const request = {
+      spreadsheetId: CONFIG_SHEET_ID,
+      range: 'A:F',
+      auth: client };
+    return sheets.spreadsheets.values.get(request, (err, response) => {
+        if (err) {
+          console.error(`The API returned an error: ${err}`);
+          return reject(err);
+        }
+        return resolve(response.data);
+      });
+  });
+}
+
+// HTTPS function to write new data to CONFIG_DATA_PATH, for testing
+exports.testsheetwrite = functions.https.onRequest(async (req, res) => {
+  // Talk to sheets.
+  const client = await getAuthorizedClient();
+  const data = await getSpreadsheet();
+
+  res.send(data);
+});
