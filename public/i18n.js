c47e9834121ff01be6ca38e9aa37bb7ed6239f2d
Nathanael Burt <nathanael.burt@gmail.com>
2020-03-28 21:41:58 -0400

    Fix missing strings, add Italian and German to locales dropdown (#333)
    * fix class for google maps placeholder
    
    * enable italian and german in dropdown
    
    * add string instructions
    
    * Smartling translations are completed for PR#333 from missing_strings (#334)
    
    * File public/i18n/en.json was translated to it-IT locale
    
    * File public/i18n/en.json was translated to de-DE locale
    
    * File public/i18n/en.json was translated to fr-FR locale
    
    * remove kleenex filter per #i18n request

--- public/i18n.js
+++ public/i18n.js
@@ -1,25 +1,27 @@
-// NOTE: Trailing commas in this object are fine, per ECMAScript 5 (circa 2009).
-
 const locales = {
   "qqq": "i18n/qqq.json",
   "en": "i18n/en.json",
   "fr": "i18n/fr-fr.json",
-  "fr-FR": "i18n/fr-fr.json"
-}
+  "fr-FR": "i18n/fr-fr.json",
+  "de": "i18n/de-de.json",
+  "de-DE": "i18n/de-de.json",
+  "it": "i18n/it-it.json",
+  "it-IT": "i18n/it-it.json",
+};
 
 $(function () {
   const searchParams = new URLSearchParams((new URL(window.location)).search);
   const locale = searchParams.get('locale');
 
-  const init = function() {
+  const init = function () {
     // translate static elements and initialize translations
     // then, remove spinner and show page content
     $('.i18n').i18n();
     window.i18nReady = true;
     $('html').trigger('i18n:ready');
-    $('.translated-content').css({display: 'block'});
-    $('.languages-loading').css({display: 'none'});
-  }
+    $('.translated-content').css({ display: 'block' });
+    $('.languages-loading').css({ display: 'none' });
+  };
 
   if (locale) {
     $.i18n({ locale: locale }).load(locales).done(init);
