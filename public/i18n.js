d2d939a8056ac02091dcc587a58416cad3561c90
Nate Burt <nate@squarerootsgrow.com>
2020-03-30 22:33:52 -0400

    only load required translation file

--- public/i18n.js
+++ public/i18n.js
@@ -1,6 +1,6 @@
 const locales = {
-  "qqq": "i18n/qqq.json",
   "en": "i18n/en.json",
+  "en-US": "i18n/en.json",
   "fr": "i18n/fr-fr.json",
   "fr-FR": "i18n/fr-fr.json",
   "de": "i18n/de-de.json",
@@ -11,10 +11,33 @@ const locales = {
   "es-ES": "i18n/es-es.json"
 };
 
-$(function () {
+const getLocale = (detectedLocale) => {
   const searchParams = new URLSearchParams((new URL(window.location)).search);
-  const locale = searchParams.get('locale');
+  return searchParams.get('locale') || detectedLocale;
+};
 
+const activeLocaleMap = (locale) => {
+  // if user explicitly tells us what they want only return that json file
+  if (locale && locales[locale]) {
+    return {
+      [locale]: locales[locale]
+    };
+  }
+
+  const language = locale.split('-')[0];
+
+  // if we do not have the locale, check if we have an international language version
+  if (locale && locales[language]) {
+    return {
+      [language]: locales[language]
+    };
+  }
+
+  // we don't know what the user wants, load english
+  return { en: locales.en };
+};
+
+$(function () {
   const init = function () {
     // translate static elements and initialize translations
     // then, remove spinner and show page content
@@ -25,9 +48,13 @@ $(function () {
     $('.languages-loading').css({ display: 'none' });
   };
 
+  const detectedLocale = $.i18n().locale;
+  const locale = getLocale(detectedLocale);
+  const localeMap = activeLocaleMap(locale);
+
   if (locale) {
-    $.i18n({ locale: locale }).load(locales).done(init);
+    $.i18n({ locale: locale }).load(localeMap).done(init);
   } else {
-    $.i18n().load(locales).done(init);
+    $.i18n().load(localeMap).done(init);
   }
 });
