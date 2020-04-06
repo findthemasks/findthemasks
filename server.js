88bb82a95ceb668c79f7fcb3d73f8a29bc3f6db8
Vlad Dzhabarov <vdzhabarov@tractionguest.com>
2020-04-06 13:33:49 -0700

    Add procfile, use latest packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = new express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
