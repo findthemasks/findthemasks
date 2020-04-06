227800b43f9e086964d2777cb7f38500b4f39e9f
Nathanael Burt <nathanael.burt@gmail.com>
2020-04-06 08:36:04 -0400

    Fixes #446: translate enum values from forms (#447)
    * Fixes #446: translate enum values from forms
    
    * Smartling translations are completed for PR#447 from translate_enums (#448)
    
    * File public/i18n/en.json was translated to de-DE locale
    
    * File public/i18n/en.json was translated to zh-TW locale
    
    * File public/i18n/en.json was translated to ko-KR locale
    
    * File public/i18n/en.json was translated to es-ES locale
    
    * File public/i18n/en.json was translated to it-IT locale
    
    * File public/i18n/en.json was translated to pl-PL locale
    
    * File public/i18n/en.json was translated to ru-RU locale
    
    * File public/i18n/en.json was translated to pt-PT locale
    
    * File public/i18n/en.json was translated to hi-IN locale
    
    * File public/i18n/en.json was translated to ar-MA locale
    
    * File public/i18n/en.json was translated to fr-FR locale
    
    * File public/i18n/en.json was translated to ja-JP locale
    
    * consolidate enum translation + filter logic
    
    * Smartling translations are completed for PR#447 from translate_enums (#473)
    
    * File public/i18n/en.json was translated to de-DE locale
    
    * File public/i18n/en.json was translated to zh-TW locale
    
    * File public/i18n/en.json was translated to ko-KR locale
    
    * File public/i18n/en.json was translated to es-ES locale
    
    * File public/i18n/en.json was translated to it-IT locale
    
    * File public/i18n/en.json was translated to ru-RU locale
    
    * File public/i18n/en.json was translated to pl-PL locale
    
    * File public/i18n/en.json was translated to pt-PT locale
    
    * File public/i18n/en.json was translated to hi-IN locale
    
    * File public/i18n/en.json was translated to ar-MA locale
    
    * File public/i18n/en.json was translated to fr-FR locale
    
    * File public/i18n/en.json was translated to ja-JP locale
    
    * fix i18n load order
    
    * refactor

---
+++ public/formEnumLookups.js
@@ -0,0 +1,75 @@
+export const FILTER_ITEMS = {
+  'n95s': {
+    name: 'ftm-accept-item-n95s',
+    isSet: false
+  },
+  'surgical masks': {
+    name: 'ftm-accept-item-surgical-masks',
+    isSet: false
+  },
+  'homemade masks (specify type in other)': {
+    name: 'ftm-accept-item-homemade-masks',
+    isSet: false
+  },
+  'face shields': {
+    name: 'ftm-accept-item-face-shields',
+    isSet: false
+  },
+  'faceshields - 3d-printed or makerspace (specify type in other)': {
+    name: 'ftm-accept-item-face-shields-makerspace',
+    isSet: false
+  },
+  'disposable booties': {
+    name: 'ftm-accept-item-disposable-booties',
+    isSet: false
+  },
+  'safety goggles': {
+    name: 'ftm-accept-item-goggles',
+    isSet: false
+  },
+  'gloves': {
+    name: 'ftm-accept-item-gloves',
+    isSet: false
+  },
+  'gowns': {
+    name: 'ftm-accept-item-gowns',
+    isSet: false
+  },
+  'coveralls/bunny suits': {
+    name: 'ftm-accept-item-coveralls-bunny-suits',
+    isSet: false
+  },
+  'hand sanitizer': {
+    name: 'ftm-accept-item-sanitizer',
+    isSet: false
+  },
+  'disinfecting wipes': {
+    name: 'ftm-accept-item-disinfecting-wipes',
+    isSet: false
+  },
+  'thermometers': {
+    name: 'ftm-accept-item-thermometers',
+    isSet: false
+  },
+  'caprs': {
+    name: 'ftm-accept-item-caprs',
+    isSet: false
+  },
+  'Walkie-talkies/baby monitors': {
+    name: 'ftm-accept-item-walkie-talkies',
+    isSet: false
+  },
+  'nasopharyngeal swabs': {
+    name: 'ftm-accept-item-nasopharyngeal-swabs',
+    isSet: false
+  }
+};
+
+export const ENUM_MAPPINGS = Object.assign({}, FILTER_ITEMS, {
+  'yes': {
+    name: 'ftm-open-packages-yes'
+  },
+  'no': {
+    name: 'ftm-open-packages-no'
+  }
+});
