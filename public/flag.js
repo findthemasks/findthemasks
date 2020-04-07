bcd2f700bec36eb386b3767dbe214982653323d7
Yuhong Wang <ywang@watersmartsoftware.com>
2020-04-07 10:48:58 -0700

    use different lib, cleanup things a bit

--- public/flag.js
+++
@@ -1,5 +0,0 @@
-$(window).on( "load", function() {
-  // get url path and strip leading/trailing slashes, if no path, assume US
-  var countryCode = window.location.pathname.replace(/^\/|\/$/g, '') || 'us';
-  $('#flag-icon-span').removeClass().addClass('flag-icon flag-icon-' + countryCode);
-});
