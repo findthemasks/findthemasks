e6464c4a8ac3b702149e8f6510ac9c1ec328b0fd
Saikat Bhadra <saikat.bhadra@gmail.com>
2020-04-07 23:33:23 -0700

    dont send local errors to airbrake

--- public/airbrake.js
+++ public/airbrake.js
@@ -1,13 +1,19 @@
 // TODO we should have the environment change so we don't alert on local dev machines
+let environment;
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  environment = 'local'
+} else {
+  environment = 'production'
+}
 const airbrake = new airbrakeJs.Client({
   projectId: 266956,
   projectKey: '2c966fe44a092a4dfe2abeb19c9e112d',
-  environment: 'production'
+  environment: environment
 });
 try {
   // This will throw if the document has no head tag
   document.head.insertBefore(document.createElement("style"), null);
-} catch(err) {
+} catch (err) {
   airbrake.notify(err);
   throw err;
 }
