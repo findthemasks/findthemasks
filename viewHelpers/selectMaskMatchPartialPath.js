b3aeeea24f52ca51d3f4b52c3ed9ec34e59afa59
Yuhong Wang <ywang@watersmartsoftware.com>
2020-04-14 19:03:02 -0700

    add link to mask match

---
+++ viewHelpers/selectMaskMatchPartialPath.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+module.exports = (currentCountry) => {
+  try {
+    if (fs.existsSync(`./views/partials/countries/${currentCountry}/mask_match.handlebars`)) {
+      return `countries/${currentCountry}/mask_match`;
+    } else {
+      return 'countries/default/mask_match';
+    }
+  } catch (err) {
+    return 'countries/default/mask_match';
+  }
+};
+
