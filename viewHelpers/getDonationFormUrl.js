4898735e092867fb3f232b7dd57e328b7fcde9d6
Derek Westcott <drwestco@gmail.com>
2020-04-23 18:00:48 -0700

    DRY - indirect via language code

--- viewHelpers/getDonationFormUrl.js
+++ viewHelpers/getDonationFormUrl.js
@@ -1,67 +1,55 @@
 const DONATION_FORMS_BY_COUNTRY = {
   at: {
-    // default: at
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLSeUzUPg9KZAQbMRCYV4ZrOI2hJ3RJ0oqp73XIIBlZjC1shGSA/viewform',
+    defaultForm: 'de',
     de: 'https://docs.google.com/forms/d/e/1FAIpQLSeUzUPg9KZAQbMRCYV4ZrOI2hJ3RJ0oqp73XIIBlZjC1shGSA/viewform'
   },
   ca: {
-    // default: en
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLSf5JAiAikzMEEw86eyjoRMH5AFlwaMrOmjjlr3vGcL5RrJt9A/viewform',
+    defaultForm: 'en',
     en: 'https://docs.google.com/forms/d/e/1FAIpQLSf5JAiAikzMEEw86eyjoRMH5AFlwaMrOmjjlr3vGcL5RrJt9A/viewform',
     fr: 'https://docs.google.com/forms/d/e/1FAIpQLSeM2Jt5zudVG9_IxCT0pXluTs4eHq7_p3X95klGCHSSSaDEFg/viewform'
   },
   ch: {
-    // default: de
-    defaultForm: 'https://docs.google.com/forms/d/1adXa1aB0-W0sFCS3m246QdAuoseisTB7zYUrHU3MljQ/viewform',
+    defaultForm: 'de',
     de: 'https://docs.google.com/forms/d/1adXa1aB0-W0sFCS3m246QdAuoseisTB7zYUrHU3MljQ/viewform',
     en: 'https://docs.google.com/forms/d/1iIS_6kp2gOV7oBhVRVUXbGMNY1vbmPeCw8E3fq3eskQ/viewform',
     fr: 'https://docs.google.com/forms/u/1/d/e/1FAIpQLSccTTMijFuFX_qimh9YMyHjInlsv7NITLfQ-LDj61aKVIV5hw/viewform',
     it: 'https://docs.google.com/forms/d/1E_X9JJcMCJAg69imKsrTTItbnKPcALq3o8vV99st3hs/viewform'
   },
   de: {
-    // default: de
-    defaultForm: 'https://docs.google.com/forms/d/16YAclvnp5C5dUx64UClIkgjvpANSqubcl3gkhdRQ1A0/viewform',
+    defaultForm: 'de',
     de: 'https://docs.google.com/forms/d/16YAclvnp5C5dUx64UClIkgjvpANSqubcl3gkhdRQ1A0/viewform'
   },
   es: {
-    // default: es
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLSeSfhRSGcSuxW02Ag8WtcWyrN8sNKmh14qd7UgkYXJrSEAGYg/viewform',
+    defaultForm: 'es',
     es: 'https://docs.google.com/forms/d/e/1FAIpQLSeSfhRSGcSuxW02Ag8WtcWyrN8sNKmh14qd7UgkYXJrSEAGYg/viewform'
   },
   fr: {
-    // default: fr
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLScys-wlfSEwCLNa5dnJIIR7LTdh7e3fpha7SL7A2jvJok8Tog/viewform',
+    defaultForm: 'fr',
     en: 'https://docs.google.com/forms/d/1nyejh2JL0YtOkMw87UtMDKXY8oVcJQnfEyusGjNVpmA/viewform',
     fr: 'https://docs.google.com/forms/d/e/1FAIpQLScys-wlfSEwCLNa5dnJIIR7LTdh7e3fpha7SL7A2jvJok8Tog/viewform'
   },
   gb: {
-    // default: en
-    defaultForm: 'https://docs.google.com/forms/d/11u77lVmIAt-B0245uD09UXEFV7XNSJo4op_DhoS5kfY/viewform',
+    defaultForm: 'en',
     en: 'https://docs.google.com/forms/d/11u77lVmIAt-B0245uD09UXEFV7XNSJo4op_DhoS5kfY/viewform'
   },
   in: {
-    // default en
-    defaultForm: 'https://docs.google.com/forms/u/1/d/1ctE1739x2S3STlL-1eNpnhlBWzLvgDNXrdeutzYCKvo/viewform',
+    defaultForm: 'en',
     en: 'https://docs.google.com/forms/u/1/d/1ctE1739x2S3STlL-1eNpnhlBWzLvgDNXrdeutzYCKvo/viewform'
   },
   it: {
-    // default: it
-    defaultForm: 'https://docs.google.com/forms/d/19HDfBSo4tOdV_gBWamCyR2MAogc5x24iG43WftiODGQ/viewform',
+    defaultForm: 'it',
     it: 'https://docs.google.com/forms/d/19HDfBSo4tOdV_gBWamCyR2MAogc5x24iG43WftiODGQ/viewform'
   },
   pl: {
-    // default: pl
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLScrTaeroICRp7XKJZsQcY2zchLBd-vBrG2mnH7BRg6gORyTFg/viewform',
+    defaultForm: 'pl',
     pl: 'https://docs.google.com/forms/d/e/1FAIpQLScrTaeroICRp7XKJZsQcY2zchLBd-vBrG2mnH7BRg6gORyTFg/viewform'
   },
   pt: {
-    // default: pt
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLScbBpYJj6WTlHhdMsFjHnBGzD_Ge_quqNv42iJiUhGst1GPrg/viewform',
+    defaultForm: 'pt',
     pt: 'https://docs.google.com/forms/d/e/1FAIpQLScbBpYJj6WTlHhdMsFjHnBGzD_Ge_quqNv42iJiUhGst1GPrg/viewform'
   },
   us: {
-    // default: en
-    defaultForm: 'https://docs.google.com/forms/d/e/1FAIpQLSfgCpK5coPVFC6rJrE7ZhimiZuDoEaL6fo6gYqxsN_FIpJZhg/viewform',
+    defaultForm: 'en',
     en: 'https://docs.google.com/forms/d/e/1FAIpQLSfgCpK5coPVFC6rJrE7ZhimiZuDoEaL6fo6gYqxsN_FIpJZhg/viewform'
   }
 };
@@ -81,5 +69,5 @@ module.exports = (country, locale) => {
     }
   }
 
-  return form || forms.defaultForm;
+  return form || forms[forms.defaultForm];
 };
