a5d6f6b73f0ef8334736c2ec6b759b7497255e5a
Nick Gottlieb <ngottlieb@gmail.com>
2020-04-24 21:46:38 -0700

    fix country code parsing to allow countries without trailing slash

--- client/getCountry.js
+++ client/getCountry.js
@@ -4,7 +4,7 @@ import countries from './countries.js';
 export function getFirstPathPart() {
   const url = new FtmUrl(window.location);
   const directories = url.pathname.split('/');
-  if (directories.length > 2) {
+  if (directories.length >= 2) {
     return directories[1];
   }
 
