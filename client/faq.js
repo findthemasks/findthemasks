48db2673e327ae63d3e3ea7b9d7a0b6959048112
Nick Gottlieb <ngottlieb@gmail.com>
2020-04-26 15:09:43 -0700

    add collapse logic and icons for FAQ

--- client/faq.js
+++ client/faq.js
@@ -1,3 +1,15 @@
 $(() => {
-  console.log('ON THE FAQ');
+  // initializes collapse toggling on FAQ page
+  // it's a bit markup dependent, but this approach seemed
+  // better than assigning a data-target and id to every question/answer
+  // pair
+  $('.faq-question dt').on('click', (event) => {
+    const $header = $(event.currentTarget);
+    if ($header.hasClass('open')) {
+      $header.removeClass('open');
+    } else {
+      $header.addClass('open');
+    }
+    $header.next('dd').collapse('toggle');
+  });
 });
