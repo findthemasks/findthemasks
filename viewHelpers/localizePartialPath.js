e6473bc00b61f9d8d96ccc3e5fbb5ae5a1659fe7
Nick Gottlieb <ngottlieb@gmail.com>
2020-05-02 15:38:56 -0700

    add "europe" regional spotlight partial

--- viewHelpers/localizePartialPath.js
+++ viewHelpers/localizePartialPath.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 module.exports = (partialName, country) => {
+  const isEuropean = ['at', 'ch', 'de', 'es', 'fr', 'uk', 'it', 'pl', 'pt'].includes(country);
+
   try {
     if (fs.existsSync(`./views/partials/countries/${country}/${partialName}.handlebars`)) {
       return `countries/${country}/${partialName}`;
+    }
+    if (isEuropean && fs.existsSync(`./views/partials/countries/europe/${partialName}.handlebars`)) {
+      return `countries/europe/${partialName}`;
     }
     return `countries/default/${partialName}`;
   } catch (err) {
