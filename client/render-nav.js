a6e1d49240b24c169f97c292594c385ecea56a3c
Nate Burt <nate@squarerootsgrow.com>
2020-05-04 21:21:47 -0400

    fix dom elements loop

--- client/render-nav.js
+++ client/render-nav.js
@@ -5,16 +5,18 @@ $(() => {
   $('html').on('i18n:ready', () => {
     const countryLinks = document.getElementsByClassName('countries-dropdown-item');
 
-    countryLinks.forEach((countryLink) => {
+    for (let i = 0; i < countryLinks.length; i++) {
+      const countryLink = countryLinks[i];
       const countryCode = countryLink.getAttribute('data-code');
       countryLink.addEventListener('click', () => sendEvent('i18n', 'set-country', countryCode));
-    });
+    }
 
     const localeLinks = document.getElementsByClassName('locales-dropdown-item');
 
-    localeLinks.forEach((localeLink) => {
+    for (let i = 0; i < localeLinks.length; i++) {
+      const localeLink = localeLinks[i];
       const localeCode = localeLink.getAttribute('data-code');
       localeLink.addEventListener('click', () => sendEvent('i18n', 'set-locale', localeCode));
-    });
+    }
   });
 });
