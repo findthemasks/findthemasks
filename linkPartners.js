1e99152ff12d80dd78e099c2e3bb2a6f883c906f
Steve Mattingly <mattinglys@dewv.edu>
2020-05-25 21:49:21 -0400

    Tweak examples for partner links

--- linkPartners.js
+++ linkPartners.js
@@ -1,15 +1,14 @@
 /*
 #findthemasks provides this file for partners to configure "callback" links to their sites.
 
-The example entry below means that all requests to FTM's embed with HTTP referer from example.com
-will receive responses with embedded links to example.com/target/path/for/links?id=X where X is a
-unique numeric identifier for the PPE requester. Full information on that requester can be accessed
-in findthemasks.com/data.json, matching X to the row id.
+The dewv.net entry below means that all requests to FTM's embed with HTTP referer from dewv.net
+will receive responses with embedded links to dewv.net/ftm?id=X where X is a unique numeric
+identifier for the PPE requester. Full information on that requester can be accessed in
+findthemasks.com/data.json, matching X to the row id.
 
-See /scss/partner-link-icons.data.svg.scss to style the links.
+Partners should also edit /scss/partner-link-icons.data.svg.scss to style their links.
 */
 
 module.exports = {
-  'example.com': '/target/path/for/links',
   'dewv.net': '/ftm',
 };
