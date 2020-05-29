11d48a6e6431d611e8c67d58716756d674d8f725
Nick Gottlieb <ngottlieb@gmail.com>
2020-05-29 09:29:22 -0700

    fix requester/requesters typo

--- client/locations-list-map.js
+++ client/locations-list-map.js
@@ -912,9 +912,10 @@ function getFlatFilteredEntries(data, filters) {
 function createZoomToMarkerIcon() {
   const headerZoomLink = ce('div', 'icon icon-search entry-zoom-link');
   let tooltipText;
+  console.log(gDataset);
   if (gDataset === 'makers') {
     tooltipText = $.i18n('ftm-makers-zoom-tooltip');
-  } else if (gDataset === 'requesters') {
+  } else if (gDataset === 'requester') {
     tooltipText = $.i18n('ftm-requesters-zoom-tooltip');
   } else {
     tooltipText = $.i18n('ftm-default-zoom-tooltip');
