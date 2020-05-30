f66c2305ea3f2e1e814948757a9826df91f79f88
Albert J. Wong <awong.dev@gmail.com>
2020-05-30 01:35:26 -0700

    Add cloud function for executing email link commands.
    This set of functions takes an encrypted json blob, and on
    successfuly decryption sends a signed version of it to the public
    AppScript endpoint.  The AppScript is expected to verify the
    signature and timestamp and on success, execute the command.
    
    Detail errors are not reported back from the function to prevent
    implementation details from leaking.

---
+++ functions/constants.js
@@ -0,0 +1,18 @@
+const GETUSPPE_AFFILIATES_SHEET_ID = '1WJC_08ajFT77C5peWPTnYyWARIU3PT8BXAxoGk90bdA';
+
+const SHEETS = {
+  at: '19gKSyKmT4yU7F32R3lBM6p0rmMJXusX_uMYDq1CMTIo',
+  ca: '1STjEiAZVZncXMCUBkfLumRNk1PySLSmkvZuPqflQ1Yk',
+  ch: '1mFbEzrWW8XLfrkAL0eCzGd1pCVNl6-QUxkoeubtdbPI',
+  de: '1qiR4JRvPrbOwlPnEXCoUFWpfZtV9xadjBTCOhVy-dJM',
+  es: '1S3FO5gmXUvQdsGXjC0hBSUxBJZoaUzy1ctylTjUGOlM',
+  fr: '1YGWlGPOfJFEsUP6VTFCVohlsHxKMYA5HppatbLwNBVk',
+  uk: '1qPUdGOEZl-c8sQ6Vlm7h48OZScBOVGjzz88AyGCQvEc',
+  in: '1w6tm1IvVeR3g_4SvXQRlFHAtudgHvldUFEJlj1p4WDk',
+  it: '1YHt6G1ghcXrRqXflevxHAwg2XOXmG2Cym6nSS5vXe7Q',
+  pl: '10EsvvozwLTQpn0ejvPjZSWt9lve3fnMHBHltW-v_zkY',
+  pt: '1QnyjUUBT_P476dEl0WfQwVnW15Ie7ogty7DiOkMhHLo',
+  us: '1GwP7Ly6iaqgcms0T80QGCNW4y2gJ7tzVND2CktFqnXM',
+};
+
+module.exports = { SHEETS, GETUSPPE_AFFILIATES_SHEET_ID };
