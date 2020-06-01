50a1f0379abf57ec38e80a4829c0f9b407633ea8
Steve Mattingly <mattinglys@dewv.edu>
2020-06-01 18:01:52 -0400

    Eliminate IE11 errors due to newer syntax

--- public/airbrake.js
+++ public/airbrake.js
@@ -2,7 +2,7 @@
 
 // TODO we should have the environment change so we don't alert on local dev machines
 let environment;
-const { hostname } = window.location;
+const hostname = window.location.hostname; // eslint-disable-line prefer-destructuring
 if (hostname === 'localhost' || hostname === '127.0.0.1' || hostname === 'local.findthemasks.com') {
   environment = 'local';
 } else {
@@ -14,7 +14,7 @@ if (environment === 'production') {
   const airbrake = new airbrakeJs.Client({
     projectId: 266956,
     projectKey: '2c966fe44a092a4dfe2abeb19c9e112d',
-    environment,
+    environment: environment, // eslint-disable-line object-shorthand
   });
   try {
     // This will throw if the document has no head tag
