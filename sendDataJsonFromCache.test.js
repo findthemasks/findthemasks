32e9af7e956589004910e7ed6f91e2344a3c527b
zhuochenwang <joshwang@wustl.edu>
2020-07-06 16:57:59 -0500

    More lint fixes

--- sendDataJsonFromCache.test.js
+++ sendDataJsonFromCache.test.js
@@ -1,19 +1,19 @@
 jest.mock('https');
 jest.mock('./sendDataJson', () => ({
-  sendDataJson: (cache, countryCode, res) => { return cache[countryCode].data; },
+  sendDataJson: (cache, countryCode, res) => cache[countryCode].data,
 }));
 
 const regeneratorRuntime = require('regenerator-runtime');
 const https = require('https');
 const { sendDataJsonFromCache } = require('./rootRoutes');
 
-test ('Testing caching logic will not request new data if cache has not expired', () => {
+test('Testing caching logic will not request new data if cache has not expired', () => {
   const today = new Date();
   const cache = {
-    US : {
-      expires_at: today.setDate(today.getDate()+1),
+    US: {
+      expires_at: today.setDate(today.getDate() + 1),
     },
-  }
-  expect(sendDataJsonFromCache(cache, null,'US',null)).toBeUndefined();
+  };
+  expect(sendDataJsonFromCache(cache, null, 'US', null)).toBeUndefined();
 });
 
