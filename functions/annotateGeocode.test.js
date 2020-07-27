8adb7e36921688495e42531831025efb5202c287
zhuochenwang <joshwang@wustl.edu>
2020-07-27 16:47:09 -0500

    Added templates to fakeData.js and removed tests

--- functions/annotateGeocode.test.js
+++ functions/annotateGeocode.test.js
@@ -1,77 +1,54 @@
 const mockIndex = require ('./index.js').testRefactor;
 const mockGeocode = require('./geocode.js').methods;
-const mockFirebaseFunctions = require('firebase-functions');
-const mockAdmin = require ('firebase-admin');
-const mockBottleneck = require('bottleneck');
 const mockClient = require("@googlemaps/google-maps-services-js").Client;
-const {notApprovedMissingGeocode, missingColumns, approvedNoLatLng, noAnnotation, fakeGeocode, mockMapsResponse, fakeIndices, fakeWriteBack, fakeColumns, fakeSheetID} = require('./unittest/fakeData.js');
+const {originalDataTemplate, geocodeTemplate, notApprovedMissingGeocode, approvedNoLatLng, noAnnotation, fakeGeocode, mockMapsResponse, fakeIndices, fakeWriteBack, fakeColumns, fakeSheetID} = require('./unittest/fakeData.js');
 const regeneratorRuntime = require('regenerator-runtime');
-const fakeData = require('./unittest/fakeData.js');
 
-// Due to limitations with jest mocking need to declare some mock function to be used for module mocks
-const mockFirebaseVal = jest.fn(() => {
-    return {
-        geocode: 'Cached geocode',
-    };
-});
-const mockFirebaseSet = jest.fn();
-let mockExist = true;
-
-
-// Mocking out firebase-functions to have predetermined config variables which probably wont get used
-jest.mock('firebase-functions', () => {
-    return {
-        config: jest.fn(() => {
-            return {
-                googleapi: 'fakeGoogleAPi',
-                findthemasks: {
-                    geocode_key: 'Hello',
-                }
-            };
-        })
-    };
-});
-// Mocked out any possible reference to the database. This is used to test functionality within geocodeAddress
-jest.mock('firebase-admin', () => {
-    return {
-        database: jest.fn(() => ({
-            ref: jest.fn(() => {
-                return {
-                    once: jest.fn(() => {
-                        return {
-                            val: mockFirebaseVal,
-                            exists: jest.fn(() => mockExist),
-                        };
-                    }),
-                    set: mockFirebaseSet
-                }
-            })
-        })),
-        initializeApp: jest.fn(),
-    };
-});
-// Mocked out the entire bottleneck that controls our call to maps API
-jest.mock('bottleneck', () => {
-    return jest.fn().mockImplementation(() => {
-        return {
-            schedule: jest.fn(() => {
-                return mockMapsResponse;
-            })
-        }
-    });
-});
-
-// test ('End to end testing to make sure every function gets ran once on an address that needs annotation', async() => {
-//     const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockImplementation(() => Promise.resolve(fakeGeocode));
-//     const data = await mockIndex.annotateGeocode(approvedNoLatLng);
-//     expect(data).toStrictEqual({
-//         numGeocodes : 1,
-//         numWritebacks : 1,
-//     });
+// test('Testing whether or not we are able to find the column of corresponding labels', () => {
+//     const missingColumns = JSON.parse(JSON.stringify(originalDataTemplate.values[1]));
+//     missingColumns[20]= "";
+//     expect(() => {mockIndex.getIndexColumn(missingColumns)}).toThrow();
 // });
 
-test('Testing whether or not we are able to find the column of corresponding labels', () => {
-    expect(() => {mockIndex.getIndexColumn(missingColumns)}).toThrow();
+describe('Testing functionality for createGeocodePromises() + doGeocode()', () => {
+    // test('With multiple entries, code should return an array of promises for each entry', async() => {
+    //     const copyNoAnnotation = JSON.parse(JSON.stringify(noAnnotation));
+    //     const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockResolvedValue(fakeGeocode.complete_1);
+    //     const { promises } = mockIndex.createGeocodePromises(copyNoAnnotation, fakeIndices);
+    //     expect(promises.length).toEqual(3);
+    // });
+    test('On a successful geocodeAddress() call, doGeocode() should be calling callback and updating entry and geocode', async() => {
+        const successGeocode = JSON.parse(JSON.stringify(geocodeTemplate));
+        const copyOfEntry = JSON.parse(JSON.stringify(originalDataTemplate.values[2]));
+        const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockResolvedValue(successGeocode);
+        const returnGeocode = await mockIndex.doGeocode([], '', copyOfEntry, 3, true, fakeIndices);
+        successGeocode.canonical_address = null;
+        expect(returnGeocode).toBeDefined();
+        expect(returnGeocode).toEqual(successGeocode);
+        expect(copyOfEntry[fakeIndices.lat]).toEqual(successGeocode.location.lat);
+        expect(copyOfEntry[fakeIndices.lng]).toEqual(successGeocode.location.lng);
+    });
+    // test('On a failed geocodeAddress() call, no callback function is called and entry lat/lng are set to N/A', () => {
+    //     const copyOfEntry = JSON.parse(JSON.stringify(originalDataTemplate.values[2]));
+    //     const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockRejectedValue(new Error('Some error with geocodeAddress!'));
+    //     const returnGeocode = await mockIndex.doGeocode([], '', copyOfEntry, 3, true, fakeIndices);
+    // });
+    // test('With multiple entries, code should have well-defined behavior for successful and failed geocodeAddress calls', async() => {
+    //     const copyNoAnnotation = JSON.parse(JSON.stringify(noAnnotation));
+    //     const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress')
+    //         .mockResolvedValueOnce(fakeGeocode.complete_1)
+    //         .mockRejectedValueOnce(new Error('Some error with geocodeAddress!'))
+    //         .mockResolvedValueOnce(fakeGeocode.complete_2);
+    //     const { promises, to_write_back } = mockIndex.createGeocodePromises(copyNoAnnotation, fakeIndices);
+    //     await Promise.all(promises);
+    //     console.log(promises);
+    //     expect(promises.length).toEqual(3);
+    //     expect(to_write_back.length).toEqual(2);
+    //     expect(copyNoAnnotation[1][fakeIndices.lat]).toEqual('N/A');
+    //     expect(copyNoAnnotation[1][fakeIndices.lng]).toEqual('N/A');
+    //     expect(copyNoAnnotation[0][fakeIndices.lat]).toEqual(fakeGeocode.complete_1.location.lat);
+    //     expect(copyNoAnnotation[0][fakeIndices.lng]).toEqual(fakeGeocode.complete_1.location.lng);
+    // });
 });
 
 test('Testing a one-to-three correspondence between each location and their corresponding data entries in fillWriteRequest', () => {
@@ -98,52 +75,23 @@ test('Testing a one-to-three correspondence between each location and their corr
     
 });
 
-describe('Testing functionality within geocodeAddress()', () => {
-    test('If the entry is cached in realtime db, simply return it', async() => {
-        mockExist = true;
-        expect(await mockGeocode.geocodeAddress('Hello')).toBe('Cached geocode');
-    });
-    test('On a good response, cache geocode values and return them', async() => {
-        mockExist = false;
-        expect(await mockGeocode.geocodeAddress('Hello')).not.toBeUndefined();
-        expect(mockFirebaseVal).toHaveBeenCalled();
-        expect(mockFirebaseSet).toHaveBeenCalled();
-    });
-    test('On a bad response status, code should throw an error', () => {
-        mockMapsResponse.status = 300;
-        const expectedError = new Error(`status: ${mockMapsResponse.status} req: ${mockMapsResponse.config.url} ${JSON.stringify(mockMapsResponse.config.params)} result: ${mockMapsResponse.data}`);
-        expect(mockGeocode.geocodeAddress('Hello')).rejects.toEqual(expectedError);
-    });
-//     // It seems like we are still returning data even when the data is bad? Need to check with someone
-//     // test('On bad data results, code should also throw an error', () => {
-//     //     mockMapsResponse.status = 200;
-//     //     mockMapsResponse.data.results = [];
-//     //     const expectedError = new Error(`status: ${mockMapsResponse.status} req: ${mockMapsResponse.config.url} ${JSON.stringify(mockMapsResponse.config.params)} result: ${mockMapsResponse.data}`);
-//     //     expect(mockGeocode.geocodeAddress('Hello')).rejects.toEqual(expectedError);
-//     // });
-});
+// describe('Testing the ability to write back to Google Sheets', () => {
+//     test('All promises should be resolved before we make the write_request', () => {
+//         const blankFillWriteRequest = jest.spyOn(mockGeocode, 'fillWriteRequest')
+//             .mockImplementation((to_write_back, columns, COMBINED_WRITEBACK_SHEET) => {
+//                 expect(to_write_back.length).toBe(1);
+//             });
+//         const copyNoAnnotation = JSON.parse(JSON.stringify(approvedNoLatLng));
+//         const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockResolvedValue(fakeGeocode.complete_1);
+//         mockIndex.annotateGeocode(copyNoAnnotation);
+//     });
+// });
 
-describe('Testing functionality for createGeocodePromises() + doGeocode()', () => {
-    test('With multiple entries and succesfull geocode, code should return an array of promises for each entry', async() => {
-        const copyNoAnnotation = JSON.parse(JSON.stringify(noAnnotation));
-        const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockResolvedValue(fakeGeocode.complete_1);
-        const { promises } = mockIndex.createGeocodePromises(copyNoAnnotation, fakeIndices);
-        expect(promises.length).toEqual(3);
-    });
-    test('With multiple entries, code should have well-defined behavior for successful and failed geocodeAddress calls', async() => {
-        const copyNoAnnotation = JSON.parse(JSON.stringify(noAnnotation));
-        const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress')
-            .mockResolvedValueOnce(fakeGeocode.complete_1)
-            .mockRejectedValueOnce(new Error('Some error with geocodeAddress!'))
-            .mockResolvedValueOnce(fakeGeocode.complete_2);
-        const { promises, to_write_back } = mockIndex.createGeocodePromises(copyNoAnnotation, fakeIndices);
-        await Promise.all(promises);
-        expect(promises.length).toEqual(3);
-        expect(to_write_back.length).toEqual(2);
-        expect(copyNoAnnotation[1][fakeIndices.lat]).toEqual('N/A');
-        expect(copyNoAnnotation[1][fakeIndices.lng]).toEqual('N/A');
-        expect(copyNoAnnotation[0][fakeIndices.lat]).toEqual(fakeGeocode.complete_1.location.lat);
-        expect(copyNoAnnotation[0][fakeIndices.lng]).toEqual(fakeGeocode.complete_1.location.lng);
-    });
-
-});
+// test ('End to end testing to make sure every function gets ran once on an address that needs annotation', async() => {
+//     const spyOnGeocodeAddress = jest.spyOn(mockGeocode, 'geocodeAddress').mockImplementation(() => Promise.resolve(fakeGeocode.complete_1));
+//     const data = await mockIndex.annotateGeocode(approvedNoLatLng);
+//     expect(data).toStrictEqual({
+//         numGeocodes : 1,
+//         numWritebacks : 1,
+//     });
+// });
